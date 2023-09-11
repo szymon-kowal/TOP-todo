ee9c92930fc8de74e420bb46ee11ef554e580056
szymon-kowal <00szymon.kowal@gmail.com>
2023-09-11 14:59:37 +0200

    Rewrite function which adds buttons to page.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './style.css';
 import todo from './modules/todo.js'
 
+let db = {};
+
+function dbSaver(){
+
+}
+
+function screenControler() {
+    const buttonAddProject = document.querySelector('.add-project');
+    const buttonAddElement = document.querySelector('.add-element');
+    
+    buttonAddProject.addEventListener('click', addAddMenu);
+    buttonAddElement.addEventListener('click', addAddMenu);
+
+    function addAddMenu() {
+        let parentElement = this.parentNode;
+
+        const newDiv = document.createElement('div');
+        const buttonsDiv = document.createElement('div');
+        const buttonAdd = document.createElement('button');
+        const buttonRemove = document.createElement('button');
+
+        buttonsDiv.className = 'add-buttons';
+        buttonAdd.className = 'menu-add-button';
+        buttonRemove.className = 'menu-remove-button'
+        
+        buttonAdd.innerHTML = 'Add';
+        buttonRemove.innerHTML = 'Remove'
+
+        newDiv.className = 'add-menu';
+        newDiv.innerHTML = `<input type="text">`;
+        
+        buttonsDiv.appendChild(buttonAdd);
+        buttonsDiv.appendChild(buttonRemove);
+        newDiv.appendChild(buttonsDiv);
+        parentElement.appendChild(newDiv);
+    }
+
+    function submitToStorage() {
+        const confirmButton = 1;
+    }
+}
+
+
+screenControler();
