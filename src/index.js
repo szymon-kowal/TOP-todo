a704b5f743029e1d4d0800f0c4a3dd8d581258e5
szymon-kowal <00szymon.kowal@gmail.com>
2023-09-19 15:16:00 +0200

    Add filtering options from database.

--- src/index.js
+++ src/index.js
@@ -8,24 +8,28 @@ let db = {
 
 // Try to add flag somehow to the event listeners with addAddMenu; - done
 // Try add todos to current db - done
-// Check if todo class is made ok and how i can improve it -
+// Check if todo class is made ok and how i can improve it - done
+// Try to display one of elements from db on page - done
 // Add functionality to buttons to the left -
 // Try to connect db with web storage -
 // Make it look ok xd -
 
 function screenControler() {
+    const cardContainer = document.querySelector('.right-content');
     const buttonAddProject = document.querySelector('.add-project');
     const buttonAddElement = document.querySelector('.add-element');
-    
-    
+
+    displayFromDatabase(db, cardContainer);
+
     buttonAddProject.addEventListener('click', addAddMenu);
     buttonAddElement.addEventListener('click', addAddMenu);
 
     let flag = true;
-
     
-
     function addAddMenu() {
+
+        
+
         if (flag) {
             flag = false;
             let parentElement = this.parentNode;
@@ -104,16 +108,16 @@ function screenControler() {
                 let dueDate = dateInput.value;
                 let prority = selectedRadio ? selectedRadio.value : 'nothing';
                 let data = new todo(title, description, dueDate, prority);
-                console.log(data);
                 submitToStorage(db, 'project1', data);
                 flag = true;
                 parentElement.removeChild(newDiv);
+
+                displayFromDatabase(db, cardContainer, selectWeek);
             });
 
             buttonRemove.addEventListener('click', () => {
                 parentElement.removeChild(newDiv);
                 flag = true;
-                console.log(flag);
             });
         };
     }  
@@ -122,7 +126,73 @@ function screenControler() {
 
 function submitToStorage(database, projectName, data) {
     database[projectName].push(data);
-    console.log(database);
 };
+
+function displayFromDatabase(database, container, filterFunction = () => true) {
+    container.innerHTML = ''; // To change
+    for (const key in database) {
+        const value = database[key];
+        for (const data of database[key]) {
+            if (filterFunction(data)) {
+                container.appendChild(displayCard(data));
+            }
+        }
+    }
+}
+
+function displayCard(data) {
+    const container = document.createElement('div');
+    container.className = 'card-container';
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            const propertyDiv = document.createElement('p');
+            propertyDiv.textContent = `${data[key]}`
+            container.appendChild(propertyDiv);
+        }
+    }
+    return container;
+}
+
+// Filters to display function
+
+function selectToday(data) {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-based
+    const day = String(today.getDate()).padStart(2, '0');
+    const newData = data.dueDate.split('-');
+
+    if (year == newData[0] && month == newData[1] && day == newData[2]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function selectWeek(data) {
+    const today = new Date();
+    const nextWeek = new Date(today);
+    nextWeek.setDate(today.getDate() + 7);
+
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-based
+    const day = String(today.getDate()).padStart(2, '0');
+    
+    const newData = data.dueDate.split('-');
+    const dataDate = new Date(newData[0], newData[1] - 1, newData[2]);
+
+    if (dataDate >= today && dataDate <= nextWeek) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function selectImportant(data) {
+    return data.prority == 'Hard';
+}
+    
+
 
 screenControler();
